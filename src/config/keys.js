0a351f381f47a406258647687b1b7133106a17c2
Chris <chrislopes1403@comcast.net>
2021-02-28 15:43:35 -0500

    5th com

--- src/config/keys.js
+++ src/config/keys.js
@@ -1,16 +1,10 @@
 
 if(process.env.NODE_ENV==='production')
 {
-    //module.exports=require('./prod.js');
-    import key from './prod';
-    export default key;
-
-    console.log(process.env)
+    module.exports=require('./prod.js');
 
 }
 else
 {
-    import key from './dev';
-    export default key;
-   // module.exports=require('./dev.js');
+    module.exports=require('./dev.js');
 }
